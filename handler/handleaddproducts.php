6977a74d38885ec7c66eab89f514bc3cacec12f5
Nathalie Flores <65398774+nafnu@users.noreply.github.com>
2022-04-19 17:36:58 +0100

    commit

---
+++ handler/handleaddproducts.php
@@ -0,0 +1,95 @@
+<?php 
+
+include '../partials/connection.php';
+
+
+$namePdt = filter_input(INPUT_POST, 'namePdt',  FILTER_SANITIZE_STRING);
+$category = filter_input(INPUT_POST, 'category', FILTER_SANITIZE_STRING);
+$description = filter_input(INPUT_POST, 'description', FILTER_SANITIZE_STRING);
+$price = filter_input(INPUT_POST, 'price', FILTER_SANITIZE_NUMBER_INT);
+
+//declare the error array
+$error =[];
+
+if(!isset($name) || empty($name)){
+    $error['namePdt'] = 'Product name is required';
+}
+if(!isset($description) || empty($description)){
+    $error['description'] = 'Description is required';
+}
+if(!isset($age) || empty($age)){
+    $error['price'] = 'Price is required';
+}
+
+// $adopted = false;
+
+
+$target_dir = "../uploads/";
+
+$target_file = $target_dir.basename($_FILES["image"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+  $check = getimagesize($_FILES["image"]["tmp_name"]);
+  if($check !== false) {
+    echo "File is an image - " . $check["mime"] . ".";
+    $uploadOk = 1;
+  } else {
+    echo "File is not an image.";
+    $uploadOk = 0;
+  }
+}
+
+// Check if file already exists
+if (file_exists($target_file)) {
+  echo "Sorry, file already exists.";
+  $uploadOk = 0;
+}
+
+// Check file size
+if ($_FILES["image"]["size"] > 500000) {
+  echo "Sorry, your file is too large.";
+  $uploadOk = 0;
+}
+
+// Allow certain file formats
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+&& $imageFileType != "gif" ) {
+  echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+  $uploadOk = 0;
+}
+
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+  echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+  if (move_uploaded_file($_FILES["image"]["tmp_name"], $target_file)) {
+    echo "The file ". htmlspecialchars( basename( $_FILES["image"]["name"])). " has been uploaded.";
+  } else {
+    echo "Sorry, there was an error uploading your file.";
+  }
+}
+
+
+if(empty($error)){
+
+    $sql = "INSERT INTO products (namePdt, category_id, price, description, image ) 
+            VALUES (?,?,?,?,?)";
+
+    $stmt= $conn->prepare($sql);
+
+    $stmt->bind_param("ssiss",  $name,$category,$price, $description, $target_file);
+
+    $stmt->execute();
+    $conn->close();
+
+    header("Location: adminindex.php");
+} else{
+    echo '<pre>';
+    print_r($error);
+    echo '</pre>';
+    require_once("addproducts.php");
+}
