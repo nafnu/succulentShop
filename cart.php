7cccc0be656d3bc4b1ab30539a5ed6aa9853fe15
Nathalie Flores <65398774+nafnu@users.noreply.github.com>
2022-04-20 23:44:16 +0100

    commit

--- cart.php
+++ cart.php
@@ -18,6 +18,16 @@
       include 'partials/header.php';  //Header - Bootstrap
     ?>
 
+<!-- 
+    <style type="text/css">
+        .counter-group button, 
+        .counter-group input {
+            /* display: inline-block; */
+            /* margin : 5px; */
+            /* padding: 5px; */
+        }
+    </style> -->
+
 </head>
 
 <body>
@@ -39,7 +49,7 @@
                                 <tr class="table_head">
                                     <th scope="col-1">Product</th>
                                     <th scope="col-2"></th>
-                                    <th scope="col-3">Price</th>
+                                    <th scope="col-3">Price    </th>
                                     <th scope="col-4">Quantity</th>
                                     <th scope="col-5">Total</th>
                                 <tr>
@@ -51,10 +61,15 @@
                                             echo "<tr class='table_row'>";
                                             echo "<th scope='col-1'>".'<img src="'.$row['image'].'" class="card-img-top" style="width:60px; height:60px" />'."</th>";
                                             echo "<td scope='col-2'>".$row['namePdt']."</td>";
-                                            echo "<td scope='col-3'>€ ".$row['price']."</td>";
-                                            echo "<td scope='col-4'><button id='aumentar' onclick='carrito(this)' value='aumentar'>+</button>".$row["<p id='contador' value=''></p>"]."<button id='disminuir' onclick='carrito(this)' value='disminuir'>-</button></td>";
-                                            echo "<td scope='col-5'>€ total </td>";
-                                            echo "<td><a class='btn btn-outline-danger' href='index.php?id=".$row['id']."' role='delete'>Delete</a></td>";
+                                            echo "<td scope='col-3'>€ ".'<input id="price" style="width:50px;text-align: center;" value="'.$row['price'].'"'."</td>";
+                                            echo "<td scope='col-4'>".'<button onclick="increase()">+</button>
+                                                    <input style="width:50px;text-align: center;" type="number" min="1" step="1" value="1" id="counter">
+                                                    <button onclick="decrease()">-</button>'."</td>";
+                                            echo "<td scope='col-5'>€ ".'<input
+                                            id="total"
+                                            type="number"
+                                            style="width:50px;text-align: center;"disabled/>'."</td>";
+                                            echo "<td><a class='btn btn-outline-danger' href='index.php?id=".$row['id']."' role='delete'>Cancel</a></td>";
                                             echo "</tr>";
                                         } 
                                     }
@@ -68,25 +83,51 @@
 
 </section>	
 
-<h2 style="background-color:aquamarine;text-align:center;font-size:-webkit-xxx-large;">We're having problems now.. please, check later!</h2>
-  <br><br><br>
 
 
-<script type="text/javascript">
-                                                var valor=0;
-                                                function carrito(boton){
-                                                    var contador = document.getElementById("contador").value;
-                                                    if (boton.value=='aumentar') {
-                                                        valor++
-                                                    }else{
-                                                        valor--
-                                                    }
-                                                    document.getElementById("contador").textContent = valor;
-                                                }
 
-                                                </script>
+    <script type="text/javascript">
+
+             function $(id)
+        {
+            return document.getElementById(id);
+        }
+
+        function increase()
+        {
+            $('counter').value = parseInt($('counter').value) + 1;
+            calcular();
+        }
+
+        function decrease()
+        {
+            var counterValue = parseInt($('counter').value);
+            var newCounterValue = (counterValue)
+                ? counterValue - 1
+                : 0;
+
+            $('counter').value = newCounterValue;
+            calcular();
+        }
+
+
+        function calcular() {
+
+        $('total').value = parseInt($('counter').value) * parseInt($('price').value);
+
+        }
+
+   
+
+    </script>
   
-    <!-- Footer -->
+    
+  
+  
+  
+  
+  
+  <!-- Footer -->
   <?php
     include 'partials/footer.php'
     ?>
